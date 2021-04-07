99c45eadc5b02e18c4f6134b3d27cdfa90eae666
Brinton <brintonfoyreed@gmail.com>
2021-04-07 12:28:58 -0700

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+overwriteBestCustomer = () => bestCustomer = 'maybe bob';
+
+const leastFavoriteCustomer = 'toni';
+
+changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'bob';
+
